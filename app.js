69ef9d70e957fce4736dbec3a70d7037bddf004e
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-28 15:34:37 +0100

    feat: create crud

--- app.js
+++ app.js
@@ -2,3 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3500;
 
+const postsRouter = require('./routers/posts')
+
+app.use(express.static('public'));
+app.use('/posts', postsRouter);
+
+
+
+// HOME
+
+app.get('/', (req, res) => {
+    res.send('Benvenuto nel mio blog di cucina!');
+    console.log('Benvenuto nel mio blog di cucina!');
+});
+
+
+
+// LISTEN
+
+app.listen(port, () => {
+    console.log('server in ascolto sulla porta = ' + port);
+});
